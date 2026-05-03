f8d0d55e47ba1131d59373714c27929282efa4d3
helmut <helmut.andrey@gmail.com>
2026-05-03 11:24:17 +0500

    add docker docker-compose work+

--- app/db/models.py
+++ app/db/models.py
@@ -2,8 +2,8 @@ import uuid
 from datetime import datetime
 from enum import StrEnum, Enum
 from typing import Optional
-# from app.db.types import TimeStamp, TextContent, ID, STATUS_POST, SOURCE_TYPE, URL
-from sqlalchemy import Column, String, DateTime, ForeignKey, Text, Boolean
+from app.db.types import TimeStamp, TextContent, ID, PostStatus, SourceType, URL
+from sqlalchemy import Column, String, DateTime, ForeignKey, Text, Boolean, Enum as SQLEnum
 from sqlalchemy.orm import declarative_base, relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.sql.annotation import Annotated
 
@@ -34,7 +34,7 @@ class Post(Base):
     news_id: Mapped[str] = mapped_column(ForeignKey('news_items.id'), index=True)
     generated_text: Mapped[Optional[str]] = Column(Text)
     published_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
-    # status: str =
+    status: Mapped[PostStatus] = mapped_column(SQLEnum(PostStatus, native_enum=False), nullable=False)
     created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
 
     news_item: Mapped[Optional["NewsItem"]] = relationship("NewsItem", back_populates="posts")
@@ -43,7 +43,7 @@ class Source(Base):
     __tablename__ = "sources"
 
     id: Mapped[str] = mapped_column(nullable=False, primary_key=True, default=uuid.uuid4)
-    # type: SOURCE_TYPE
+    type: Mapped[SourceType] = mapped_column(SQLEnum(SourceType, native_enum=False), nullable=False)
     name: Mapped[str] = mapped_column(String, nullable=False)
     url: Mapped[Optional[str]] = mapped_column(nullable=True, index=False)
     enabled: bool = Column(Boolean, nullable=False, default=True)
---
+++ celery_worker.py
@@ -0,0 +1,27 @@
+from celery import Celery
+
+
+from app.config import settings
+
+celery_app = Celery(
+    broker=settings.CELERY_BROKER_URL,
+    backend=settings.CELERY_RESULT_BACKEND,
+    include=['app.tasks']
+)
+
+celery_app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+    beat_schedule={
+        'parse_news': {
+            'task': 'parse_news',
+            'schedule': settings.PARSE_INTERVAL_MINUTES * 60,
+       },
+    }
+)
+
+if __name__ == '__main__':
+    celery_app.start()
